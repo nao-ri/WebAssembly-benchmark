4229c518c783a0acd56084785907a7c857de30c4
Your Name <you@example.com>
2021-12-29 17:15:15 +0000

    add doublewasmtest about GrayToRed

--- tests/imageGrayscaleToRed.sh
+++ tests/imageGrayscaleToRed.sh
@@ -1,10 +1,10 @@
-emcc imageGrayscale.c \
+emcc imageGrayscaleToRed.c \
   -Os \
   -s WASM=1 \
   -s MODULARIZE=1 \
   -s DEMANGLE_SUPPORT=1 \
   -s ALLOW_MEMORY_GROWTH=1 \
-  -s "EXPORTED_FUNCTIONS=['_imageGrayscale', '_malloc','_free']" \
+  -s "EXPORTED_FUNCTIONS=['_imageGrayscaleToRed', '_malloc','_free']" \
   -s "EXTRA_EXPORTED_RUNTIME_METHODS=['cwrap']"\
   -s ASSERTIONS=1 \
-  -o imageGrayscale.js
+  -o imageGrayscaleToRed.js
