e94ec6878bf03ab05cc16be25de4b7f9bc3f04eb
nao-ri <whiteblue0325@gmail.com>
2022-01-23 23:44:35 +0900

    before checking dynamic link

--- tests/imageGrayscale.c
+++ tests/imageGrayscale.c
@@ -1,14 +1,4 @@
-void imageGrayscale(unsigned char *data, int width, int height) {
-  for (int i = 0, il = width * height; i < il; i++) {
-    unsigned char r = data[i*4+0];
-    unsigned char g = data[i*4+1];
-    unsigned char b = data[i*4+2];
-    data[i*4+0] = data[i*4+1] = data[i*4+2] =
-      0.2126*r + 0.7152*g + 0.0722*b;
-  }
-}
-
-// void imageGrayscale(unsigned char *data, int width, int height) {
+// int imageGrayscaleToRed(unsigned char *data, int width, int height) {
 //   for (int i = 0, il = width * height; i < il; i++) {
 //     unsigned char r = data[i*4+0];
 //     unsigned char g = data[i*4+1];
@@ -17,5 +7,32 @@ void imageGrayscale(unsigned char *data, int width, int height) {
 //     //data[i*4+0] = data[i*4+1] = data[i*4+2] =R;
 //     data[i*4+0] = R; data[i*4+1] =0; data[i*4+2] =0;
 //   }
+//    return 1;
 // }
 
+// int imageGrayscale(unsigned char *data, int width, int height) {
+//   for (int i = 0, il = width * height; i < il; i++) {
+//     unsigned char r = data[i*4+0];
+//     unsigned char g = data[i*4+1];
+//     unsigned char b = data[i*4+2];
+//     data[i*4+0] = data[i*4+1] = data[i*4+2] =
+//       0.2126*r + 0.7152*g + 0.0722*b;
+//   }
+
+//   int Redroop = imageGrayscaleToRed(data,width,height);
+//   if (Redroop){
+//     return Redroop;
+//   }
+//    return 0;
+// }
+
+int imageGrayscale(unsigned char *data, int width, int height) {
+  for (int i = 0, il = width * height; i < il; i++) {
+    unsigned char r = data[i*4+0];
+    unsigned char g = data[i*4+1];
+    unsigned char b = data[i*4+2];
+    data[i*4+0] = data[i*4+1] = data[i*4+2] =
+      0.2126*r + 0.7152*g + 0.0722*b;
+  }
+  return 0;
+}
---
+++ tests/imageGrayscalefunc2.c
@@ -0,0 +1,23 @@
+int imageGrayscale(unsigned char *data, int width, int height) {
+  for (int i = 0, il = width * height; i < il; i++) {
+    unsigned char r = data[i*4+0];
+    unsigned char g = data[i*4+1];
+    unsigned char b = data[i*4+2];
+    data[i*4+0] = data[i*4+1] = data[i*4+2] =
+      0.2126*r + 0.7152*g + 0.0722*b;
+  }
+  return 1;
+}
+
+int imageGrayscaleToRed(unsigned char *data, int width, int height) {
+  for (int i = 0, il = width * height; i < il; i++) {
+    unsigned char r = data[i*4+0];
+    unsigned char g = data[i*4+1];
+    unsigned char b = data[i*4+2];
+    unsigned char R =254;
+    data[i*4+0] = R; data[i*4+1] =0; data[i*4+2] =0;
+  }
+  return 2;
+}
+
+
